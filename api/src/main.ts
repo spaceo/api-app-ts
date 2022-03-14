f7e2593d5181da52215c4ac2b66e99e1016484f1
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-03-14 22:09:34 +0100

    minor

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,7 +8,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
+    AppModule
   );
 
 
